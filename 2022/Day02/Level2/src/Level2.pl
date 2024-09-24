ca7aeb8e60b4d6804f156f753cec95bff814f6f5
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-24 16:59:15 -0400

    2022 Day 02 Solution

--- 2022/Day02/Level2/src/Level2.pl
+++ 2022/Day02/Level2/src/Level2.pl
@@ -9,7 +9,38 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
-   
+    my $totalScore = 0;
+    foreach my $line (@inputData) {
+        if ($line =~ /(\w)\s(\w)/) {
+            my $opponentChoice = $1; # A = Rock, B = Paper, C = Scissors
+            my $myOutcome = $2; # X = Lose, Y = Draw, Z = Win
+
+            $totalScore += 3 if $myOutcome eq 'Y'; # Draw
+            $totalScore += 6 if $myOutcome eq 'Z'; # Win
+
+            # Loss
+            if ($myOutcome eq 'X') {
+                $totalScore += 3 if $opponentChoice eq 'A';
+                $totalScore += 1 if $opponentChoice eq 'B';
+                $totalScore += 2 if $opponentChoice eq 'C';
+            }
+
+            # Draw
+            if ($myOutcome eq 'Y') {
+                $totalScore += 1 if $opponentChoice eq 'A';
+                $totalScore += 2 if $opponentChoice eq 'B';
+                $totalScore += 3 if $opponentChoice eq 'C';
+            }
+
+            # Win
+            if ($myOutcome eq 'Z') {
+                $totalScore += 2 if $opponentChoice eq 'A';
+                $totalScore += 3 if $opponentChoice eq 'B';
+                $totalScore += 1 if $opponentChoice eq 'C';
+            }
+        }
+    }
+    say $totalScore;
 }
 
 run();
