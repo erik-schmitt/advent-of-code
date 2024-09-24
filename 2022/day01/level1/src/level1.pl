7d1f503aabc09759b66c419853fa162b6e94105c
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-24 09:24:45 -0400

    Change casing of file/folder structure to proper casing.
    This looks nicer and will work better if we add classes in the future.

--- 2022/day01/level1/src/level1.pl
+++ 2022/day01/level1/src/level1.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings FATAL => 'all';
 
-my $input_file = './level1/InputDataLevel1.txt';
+my $input_file = './Level1/InputDataLevel1.txt';
 open(my $in_fh, '<', $input_file) or die "Can't open '$input_file' - $!";
 my @input_data = <$in_fh>;
 
--- 2022/day01/level2/src/level2.pl
+++ 2022/day01/level2/src/level2.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings FATAL => 'all';
 
-my $input_file = './level2/InputDataLevel2.txt';
+my $input_file = './Level2/InputDataLevel2.txt';
 open(my $in_fh, '<', $input_file) or die "Can't open '$input_file' - $!";
 my @input_data = <$in_fh>;
 
