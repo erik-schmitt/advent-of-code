7630b064d609fdf1ce3d8735529c09230789652e
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-24 20:01:09 -0400

    2022 Day 03 Solution

--- 2022/Day03/Level1/src/Level1.pl
+++ 2022/Day03/Level1/src/Level1.pl
@@ -9,7 +9,38 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
-   
+    my @sharedItems;
+    foreach my $line (@inputData) {
+        my $firstString = substr($line, 0, length($line)/2);
+        my @secondString = split('', substr($line, length($line)/2));
+
+        my %firstCompartment = map {$_ => 1} split('', $firstString);
+        while (@secondString) {
+            my $item = shift @secondString;
+            if (exists $firstCompartment{$item}) {
+                push @sharedItems, $item;
+                last;
+            }
+        }
+    }
+
+    my $prioritySum = 0;
+    foreach my $itemType (@sharedItems) {
+        my $ord = ord($itemType); # Get ASCII table value
+        
+        # Convert A-Z to scores of 27-52
+        if ($ord > 64 && $ord < 91) {
+            $prioritySum += $ord - 38;
+        }
+        # Convert a-z to scores of 1-26
+        elsif ($ord > 96 && $ord < 123) {
+            $prioritySum += $ord - 96;
+        }
+        else {
+            die("Unknown ASCII value for '$itemType'");
+        }
+    }
+    say $prioritySum;
 }
 
 run();
