6f187472832f2bdfd6d51423452355c7d747e0a3
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-25 17:17:09 -0400

    WIP

--- 2022/Day05/Level1/src/Level1.pl
+++ 2022/Day05/Level1/src/Level1.pl
@@ -9,7 +9,47 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
-  
+    my %stacks;
+    my @startingConfiguration;
+    foreach my $line (@inputData) {
+        if ($line =~ /^move/) {
+            say %stacks;
+            exit;
+        }
+        elsif ($line eq '')
+        {
+            readStartingConfiguration(\%stacks, \@startingConfiguration);
+        }
+        else {
+            push @startingConfiguration, $line;
+        }
+    }
+
+    my $topElements;
+    foreach my $stack (sort keys %stacks) {
+        $topElements .= pop @{$stacks{$stack}};
+    }
+    say $topElements, "\n";
+}
+
+sub readStartingConfiguration {
+    my ($stacks_ref, $start_ref) = @_;
+    
+    # Read stack labels
+    my $line = pop @{$start_ref};
+    %{$stacks_ref} = map {$_ => []} split(' ', $line);
+
+    # Read containers
+    my $numberOfStacks = scalar keys %{$stacks_ref};
+    while (@{$start_ref}) {
+        my $line = pop @{$start_ref};
+        $line .= ' '; # So substrings don't go outside string
+        for (my $i = 0; $i < length($line); $i+3) {
+            my $container = substr($line, $i, 4);
+            push @{$stacks_ref->{$i+1}}, $container;
+            # $line = substr($line, $i+3);            
+        }
+    }
 }
 
 run();
