df17a11a3d670d2cca455a87d17c740d90c99666
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-26 14:06:40 -0400

    Y2022D06L1 solution

--- 2022/Day06/Level1/src/Level1.pl
+++ 2022/Day06/Level1/src/Level1.pl
@@ -9,7 +9,30 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
-   
+    my @buffer = split('', $inputData[0]);
+    my @lastFour;
+    my $characterCount = 0;
+    while (@buffer) {
+        my $newCharacter = shift @buffer;
+        if (@lastFour > 3) {
+            shift @lastFour;
+        }
+        push @lastFour, $newCharacter;
+        $characterCount++;
+        if (scalar @lastFour == 4 && isUnique(@lastFour)) {
+            last;
+        }
+    }
+    say $characterCount;
+}
+
+sub isUnique {
+    my @string = @_;
+    my %unique = map {$_ => 1} @string;
+    if (scalar keys %unique != scalar @string) {
+        return 0;
+    }
+    return 1;
 }
 
 run();
