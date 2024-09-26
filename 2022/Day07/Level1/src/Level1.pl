d3329e4436c54148f1cc56f01bc790d915d784ea
Erik Schmitt <erikjschmitt@outlook.com>
2024-09-26 16:15:48 -0400

    Parse filesystem

--- 2022/Day07/Level1/src/Level1.pl
+++ 2022/Day07/Level1/src/Level1.pl
@@ -9,7 +9,68 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
+    my %fileSystem;
+    my @breadCrumbs;
+    my $action;
+    foreach my $line (@inputData) {
+        if ($line =~ /^\$ cd\s(.*)/) {
+            # Change directory
+            my $name = $1;
 
+            if ($name eq '/') {
+                @breadCrumbs = ();
+                my $cwd = \%fileSystem;
+                my %dir = (
+                    'type' => 'dir',
+                );
+                $cwd->{$name} = \%dir;
+                push @breadCrumbs, $cwd->{$name};
+            }
+            elsif ($name eq '..') {
+                pop @breadCrumbs;
+            }
+            else {
+                my $cwd = $breadCrumbs[-1];
+                my %dir = (
+                    'type' => 'dir',
+                );
+                $cwd->{$name} = \%dir;
+                push @breadCrumbs, $cwd->{$name};
+            }
+            $action = 'Change directory\n';
+        }
+        elsif ($line =~ /^\$ ls/) {
+            print "$line\n";
+            $action = 'Request listing\n';
+        }
+        elsif ($line =~ /^(\d+)\s(.+)/) {
+            # Store file
+            my $size = $1;
+            my $name = $2;
+            my $cwd = $breadCrumbs[-1];
+            my %file = (
+                'size' => $size,
+                'type' => 'file',
+            );
+            $cwd->{$name} = \%file;
+            $action = 'Store file\n';
+        }
+        elsif ($line =~ /^dir\s(.+)/) {
+            # Store directory
+            my $name = $1;
+            my $cwd = $breadCrumbs[-1];
+            my %dir = (
+                'type' => 'dir',
+            );
+            $cwd->{$name} = \%dir;
+            $action = 'Store directory\n';
+        }
+        else {
+            die("Uknown file command: $line\n");
+        }
+        say "Line: $line ### Action: $action";
+    }
+    print "File system parsed\n";
 }
 
 
