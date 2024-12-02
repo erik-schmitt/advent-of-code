60aa8707dc9e9289cbfcf958799a69c1b8a7e722
Erik Schmitt <erikjschmitt@outlook.com>
2024-12-02 14:07:39 -0500

    2024 Day 02 Solution

--- 2024/Day02/Level1/src/Level1.pl
+++ 2024/Day02/Level1/src/Level1.pl
@@ -9,7 +9,36 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
+    my $safeCount = 0;
+    REPORT: foreach my $report (@inputData) {
+        my @levels = split(/\s+/, $report);
+        my $first = shift @levels;
+        my $second = shift @levels;
+        my $direction = $first < $second ? 1 : -1;
+        next REPORT unless safeLevels($first, $second, $direction);
 
+        while (@levels) {
+            $first = $second;
+            $second = shift @levels;
+            next REPORT unless safeLevels($first, $second, $direction);
+        }
+        $safeCount++;
+    }
+    say $safeCount;
+}
+
+sub safeLevels {
+    my ($first, $second, $direction) = @_;
+    my $safe = 1;
+    if ($direction > 0) {
+        $safe = 0 if $first + 4 <= $second;
+        $safe = 0 if $first >= $second;
+    }
+    else {
+        $safe = 0 if $second + 4 <= $first;
+        $safe = 0 if $second >= $first;
+    }
+    return $safe;
 }
 
 run();
--- 2024/Day02/Level2/src/Level2.pl
+++ 2024/Day02/Level2/src/Level2.pl
@@ -9,7 +9,55 @@ my @inputData = $fileReader->getFile();
 
 
 sub run {
+    my $safeCount = 0;
+    REPORT: foreach my $report (@inputData) {
+        my @levels = split(/\s+/, $report);
+        my $safe = safeReport(@levels);
 
+        if (!$safe) {
+            my $index = 0;
+            while ($index < scalar @levels) {
+                my @dampened = @levels;
+                splice(@dampened, $index, 1);
+                $safe = safeReport(@dampened);
+                last if $safe;
+                $index++;
+            }
+            next REPORT unless $safe;
+        }
+        $safeCount++;
+    }
+    say $safeCount;
+}
+
+sub safeReport {
+    my @levels = @_;
+
+    my $first = shift @levels;
+    my $second = shift @levels;
+    my $direction = $first < $second ? 1 : -1;
+    return 0 unless safeLevels($first, $second, $direction);
+
+    while (@levels) {
+        $first = $second;
+        $second = shift @levels;
+        return 0 unless safeLevels($first, $second, $direction);
+    }
+    return 1;
+}
+
+sub safeLevels {
+    my ($first, $second, $direction) = @_;
+    my $safe = 1;
+    if ($direction > 0) {
+        $safe = 0 if $first + 4 <= $second;
+        $safe = 0 if $first >= $second;
+    }
+    else {
+        $safe = 0 if $second + 4 <= $first;
+        $safe = 0 if $second >= $first;
+    }
+    return $safe;
 }
 
 run();
