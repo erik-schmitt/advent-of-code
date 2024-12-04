f5763b500c6cb2dcfe72ab4e38a63c472d36baad
Erik Schmitt <erikjschmitt@outlook.com>
2024-12-04 16:44:09 -0500

    2024 Day 04 Solution

--- 2024/Day04/Level1/src/Level1.pl
+++ 2024/Day04/Level1/src/Level1.pl
@@ -8,8 +8,78 @@ my $fileReader = FileReader->new(filename => './Level1/InputDataLevel1.txt');
 my @inputData = $fileReader->getFile();
 
 
-sub run {
+my $count = 0;
+my @searchGrid;
+my $width = scalar split('', $inputData[0]);
+my $height = scalar @inputData;
+my $length = length 'XMAS';
 
+sub run {
+    loadWordSearch();
+
+     foreach my $m (0..$height - 1) {
+        foreach my $n (0..$width - 1) {
+            if ('X' eq $searchGrid[$m][$n]) {
+                $count += findWord($m, $n);
+            }
+        }
+    }
+    say $count;
+}
+
+sub findWord {
+    my ($m, $n) = @_;
+    my $matches = 0;
+    my $validDirections = getValidDirections($m,$n);
+    foreach my $direction (sort keys %{$validDirections}){
+        $matches++ if checkForWord($direction, $m, $n);
+    }
+    return $matches;
+}
+
+sub getValidDirections {
+    my ($m, $n) = @_;
+    my %directions;
+    $directions{'east'} = 1 if $n + $length <= $width;
+    $directions{'west'} = 1 if $n + 1 - $length >= 0;
+    $directions{'north'} = 1 if $m + 1 - $length >= 0;
+    $directions{'south'} = 1 if $m + $length <= $height;
+
+    $directions{'northeast'} = 1 if exists $directions{'east'} && exists $directions{'north'};
+    $directions{'northwest'} = 1 if exists $directions{'west'} && exists $directions{'north'};
+    $directions{'southeast'} = 1 if exists $directions{'east'} && exists $directions{'south'};
+    $directions{'southwest'} = 1 if exists $directions{'west'} && exists $directions{'south'};
+    return \%directions;
+}
+
+sub checkForWord {
+    my ($direction, $m, $n) = @_;
+    my $string = '';
+
+    $string = $searchGrid[$m][$n+1] . $searchGrid[$m][$n+2] . $searchGrid[$m][$n+3] if ($direction eq 'east');
+    $string = $searchGrid[$m][$n-1] . $searchGrid[$m][$n-2] . $searchGrid[$m][$n-3] if $direction eq 'west';
+    $string = $searchGrid[$m+1][$n] . $searchGrid[$m+2][$n] . $searchGrid[$m+3][$n] if $direction eq 'south';
+    $string = $searchGrid[$m-1][$n] . $searchGrid[$m-2][$n] . $searchGrid[$m-3][$n] if $direction eq 'north';
+    $string = $searchGrid[$m+1][$n+1] . $searchGrid[$m+2][$n+2] . $searchGrid[$m+3][$n+3] if $direction eq 'southeast';
+    $string = $searchGrid[$m+1][$n-1] . $searchGrid[$m+2][$n-2] . $searchGrid[$m+3][$n-3] if $direction eq 'southwest';
+    $string = $searchGrid[$m-1][$n+1] . $searchGrid[$m-2][$n+2] . $searchGrid[$m-3][$n+3] if $direction eq 'northeast';
+    $string = $searchGrid[$m-1][$n-1] . $searchGrid[$m-2][$n-2] . $searchGrid[$m-3][$n-3] if $direction eq 'northwest';
+
+    die "Invalid direction: $direction" if $string eq '';
+    return 1 if $string eq 'MAS';
+    return 0;
+}
+
+sub loadWordSearch {
+    foreach my $m (0..$height - 1) {
+        my $line = shift @inputData;
+        my @row = split('', $line);
+
+        foreach my $n (0..$width - 1) {
+            my $current = $row[$n];
+            $searchGrid[$m][$n] = $current;
+        }
+    }
 }
 
 run();
--- 2024/Day04/Level2/src/Level2.pl
+++ 2024/Day04/Level2/src/Level2.pl
@@ -8,8 +8,59 @@ my $fileReader = FileReader->new(filename => './Level2/InputDataLevel2.txt');
 my @inputData = $fileReader->getFile();
 
 
-sub run {
+my $count = 0;
+my @searchGrid;
+my $width = scalar split('', $inputData[0]);
+my $height = scalar @inputData;
+my $length = 2;
 
+sub run {
+    loadWordSearch();
+
+     foreach my $m (0..$height - 1) {
+        foreach my $n (0..$width - 1) {
+            if ('A' eq $searchGrid[$m][$n]) {
+                $count += checkForMatch($m,$n) if validRange($m,$n);
+            }
+        }
+    }
+    say $count;
+}
+
+sub validRange {
+    my ($m, $n) = @_;
+    return 0 unless $n + $length <= $width;
+    return 0 unless $n + 1 - $length >= 0;
+    return 0 unless $m + 1 - $length >= 0;
+    return 0 unless $m + $length <= $height;
+    return 1;
+}
+
+sub checkForMatch {
+    my ($m, $n) = @_;
+
+    my %matches = (
+        'SSMM' => 1,
+        'MSMS' => 1,
+        'SMSM' => 1,
+        'MMSS' => 1,
+    );
+
+    my $string = $searchGrid[$m-1][$n-1] . $searchGrid[$m-1][$n+1] . $searchGrid[$m+1][$n-1] . $searchGrid[$m+1][$n+1];
+    return 1 if exists $matches{$string};
+    return 0;
+}
+
+sub loadWordSearch {
+    foreach my $m (0..$height - 1) {
+        my $line = shift @inputData;
+        my @row = split('', $line);
+
+        foreach my $n (0..$width - 1) {
+            my $current = $row[$n];
+            $searchGrid[$m][$n] = $current;
+        }
+    }
 }
 
 run();
