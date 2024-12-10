64aa9674fa3f58af66c069d18fff32a262330604
Erik Schmitt <erikjschmitt@outlook.com>
2024-12-10 17:32:54 -0500

    2024 Day 11 Skeleton

---
+++ 2024/Day11/Level2/src/Level2.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use v5.38;
+use lib 'c:/users/eriks/Documents/Code/advent-of-code/2024/utils/';
+use FileReader;
+use Data::Dumper;
+
+my $fileReader = FileReader->new(filename => './Level2/InputDataLevel2.txt');
+my @inputData = $fileReader->getFile();
+
+
+sub run {
+
+}
+
+run();
