5b95a64ed58e01f5cc16d3700f6bbb3f5996dc24
Erik Schmitt <erikjschmitt@outlook.com>
2024-12-11 11:31:15 -0500

    Update Template to make folder structure cleaner and .gitignore to match

--- aoc_utilities/Day_TEMPLATE/src/Level1.pl
+++ aoc_utilities/Day_TEMPLATE/src/Level1.pl
@@ -3,8 +3,9 @@ use warnings;
 use v5.38;
 use lib 'c:/users/eriks/Documents/Code/advent-of-code/YEAR/utils/';
 use FileReader;
+use Data::Dumper;
 
-my $fileReader = FileReader->new(filename => './Level1/InputDataLevel1.txt');
+my $fileReader = FileReader->new(filename => './InputData.txt');
 my @inputData = $fileReader->getFile();
 
 
